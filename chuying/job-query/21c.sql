a8e6aa7f9c9dade7872d8e4290bff1f338b3b330
ChuyingHe <heidihcy@outlook.com>
2021-08-09 21:12:01 +0200

    try, mostly will hang due to too much relations

--- chuying/job-query/21c.sql
+++ chuying/job-query/21c.sql
@@ -1,12 +1,13 @@
---11a.sql
-SELECT MIN(cn.name) AS from_company,
-       MIN(lt.link) AS movie_link_type,
-       MIN(t.title) AS non_polish_sequel_movie
+--21c.sql
+SELECT MIN(cn.name) AS company_name,
+       MIN(lt.link) AS link_type,
+       MIN(t.title) AS western_follow_up
 FROM company_name AS cn,
      company_type AS ct,
      keyword AS k,
      link_type AS lt,
      movie_companies AS mc,
+     movie_info AS mi,
      movie_keyword AS mk,
      movie_link AS ml,
      title AS t
@@ -17,7 +18,16 @@ WHERE cn.country_code !='[pl]'
   AND k.keyword ='sequel'
   AND lt.link LIKE '%follow%'
   AND mc.note IS NULL
-  AND t.production_year BETWEEN 1950 AND 2000
+  AND mi.info IN ('Sweden',
+                  'Norway',
+                  'Germany',
+                  'Denmark',
+                  'Swedish',
+                  'Denish',
+                  'Norwegian',
+                  'German',
+                  'English')
+  AND t.production_year BETWEEN 1950 AND 2010
   AND lt.id = ml.link_type_id
   AND ml.movie_id = t.id
   AND t.id = mk.movie_id
@@ -25,7 +35,11 @@ WHERE cn.country_code !='[pl]'
   AND t.id = mc.movie_id
   AND mc.company_type_id = ct.id
   AND mc.company_id = cn.id
+  AND mi.movie_id = t.id
   AND ml.movie_id = mk.movie_id
   AND ml.movie_id = mc.movie_id
-  AND mk.movie_id = mc.movie_id;
+  AND mk.movie_id = mc.movie_id
+  AND ml.movie_id = mi.movie_id
+  AND mk.movie_id = mi.movie_id
+  AND mc.movie_id = mi.movie_id;
 
--- chuying/job-query/22d.sql
+++ chuying/job-query/22d.sql
@@ -1,41 +1,53 @@
---14a.sql
-SELECT MIN(mi_idx.info) AS rating,
-       MIN(t.title) AS northern_dark_movie
-FROM info_type AS it1,
+--22d.sql
+SELECT MIN(cn.name) AS movie_company,
+       MIN(mi_idx.info) AS rating,
+       MIN(t.title) AS western_violent_movie
+FROM company_name AS cn,
+     company_type AS ct,
+     info_type AS it1,
      info_type AS it2,
      keyword AS k,
      kind_type AS kt,
+     movie_companies AS mc,
      movie_info AS mi,
      movie_info_idx AS mi_idx,
      movie_keyword AS mk,
      title AS t
-WHERE it1.info = 'countries'
+WHERE cn.country_code != '[us]'
+  AND it1.info = 'countries'
   AND it2.info = 'rating'
   AND k.keyword IN ('murder',
                     'murder-in-title',
                     'blood',
                     'violence')
-  AND kt.kind = 'movie'
+  AND kt.kind IN ('movie',
+                  'episode')
   AND mi.info IN ('Sweden',
                   'Norway',
                   'Germany',
                   'Denmark',
                   'Swedish',
-                  'Denish',
+                  'Danish',
                   'Norwegian',
                   'German',
                   'USA',
                   'American')
   AND mi_idx.info < '8.5'
-  AND t.production_year > 2010
+  AND t.production_year > 2005
   AND kt.id = t.kind_id
   AND t.id = mi.movie_id
   AND t.id = mk.movie_id
   AND t.id = mi_idx.movie_id
+  AND t.id = mc.movie_id
   AND mk.movie_id = mi.movie_id
   AND mk.movie_id = mi_idx.movie_id
+  AND mk.movie_id = mc.movie_id
   AND mi.movie_id = mi_idx.movie_id
+  AND mi.movie_id = mc.movie_id
+  AND mc.movie_id = mi_idx.movie_id
   AND k.id = mk.keyword_id
   AND it1.id = mi.info_type_id
-  AND it2.id = mi_idx.info_type_id;
+  AND it2.id = mi_idx.info_type_id
+  AND ct.id = mc.company_type_id
+  AND cn.id = mc.company_id;
 

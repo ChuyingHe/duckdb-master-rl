7069074c6fdd669e0f05085dd4f6509177d49df8
ChuyingHe <heidihcy@outlook.com>
2021-09-09 14:27:03 +0200

    Branch purpose: run simulation only for determining the optimal join order, no intermediate result available

--- chuying/main.cpp
+++ chuying/main.cpp
@@ -21,25 +21,17 @@ string readFileIntoString(const string& path) {
 }
 
 void loadTables(Connection con) {
-    // std::cout <<"\n 🌈 loadTables \n";
     for (int t = 0; t < IMDB_TABLE_COUNT; t++) {
-        //std::cout << IMDB_TABLE_NAMES[t] << ": ";
-
         con.Query(IMDB_TABLE_CREATE_SQL[t]);
         con.Query(IMDB_TABLE_FROM_CSV_SQL[t]);
-
-        /*TODO: delete the following test code*/
         auto test = con.Query(TEST_QUERY[t]);
         test->Print();
     }
 }
 
 void addIndexes(Connection con) {
-    //std::cout <<"\n 🌈 addIndexes \n";
     for (int i=0; i < IMDB_TABLE_INDEX.size(); i++) {
-        //std::cout << i<<"/"<< IMDB_TABLE_INDEX.size() <<" " << IMDB_TABLE_INDEX[i] << "\n";
         con.Query(IMDB_TABLE_INDEX[i]);
-
     }
     return;
 }
@@ -59,42 +51,17 @@ void runJOBQuerys(Connection con) {
 
             std::string job_file = entry.path().filename().string();
             std::cout <<job_file <<",";
-            /*
-            std::string job_profiling = "PRAGMA profile_output='" + getRootPath() +"/chuying/profiling/" + job_file + ".json';";
-            con.Query(job_profiling);
-            std::cout <<"entry_path" <<entry.path() <<"\n 🎄 JOB query = " << job_query <<"\n\n";*/
+
             std::string job_query = readFileIntoString(entry.path());
-            // Timer timer;
 
             auto result = con.Query(job_query);
             result->Print();
-            //double duration = timer.check();
-            //std::cout <<"time_total = " << duration <<"\n";
         }
     }
-    //printf("end of runJOBQuerys() \n");
 }
-
-/*
-#define TESTING_DIRECTORY_NAME "chuying_temp_dir"
-
-
-string TestDirectoryPath() {
-    FileSystem fs;
-    if (!fs.DirectoryExists(TESTING_DIRECTORY_NAME)) {
-        fs.CreateDirectory(TESTING_DIRECTORY_NAME);
-    }
-    return TESTING_DIRECTORY_NAME;
-}
-string TestCreatePath(string suffix) {
-    FileSystem fs;
-    return fs.JoinPath(TestDirectoryPath(), suffix);
-}
-*/
 
 bool existDB(std::string db) {
     std::string db_dir = getBuildPath() + "/" + db ;
-    //std::cout << "test = " <<db_dir << "\n";
     if (std::filesystem::exists(db_dir)) {
         std::cout<< "the db already exists";
         return true;
@@ -103,7 +70,6 @@ bool existDB(std::string db) {
 }
 
 int main() {
-
     // if persistent db exist
     auto storage_db = IMDB_DIRECTORY + "imdb";
     DuckDB db(storage_db);
@@ -114,9 +80,10 @@ int main() {
     if (!fs.DirectoryExists(storage_db)) {
         printf("create persistent db \n");
         loadTables(con);
-    }*/
+    }
+     addIndexes(con);
+     */
 
-	//addIndexes(con);
 	runJOBQuerys(con);
 
 }

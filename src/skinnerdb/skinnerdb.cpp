0ebd757ba74f1cfe640226cbf1f11c5ab8486dfe
ChuyingHe <heidihcy@outlook.com>
2021-09-18 11:26:59 +0200

    avoiding using first simulation for RL algorithm

--- src/skinnerdb/skinnerdb.cpp
+++ src/skinnerdb/skinnerdb.cpp
@@ -72,7 +72,7 @@ unique_ptr<QueryResult> SkinnerDB::CreateAndExecuteStatement(){
     //bool found_optimal_join_order = false;
     unique_ptr<LogicalOperator> rl_plan;
 
-    double prev_duration, current_duration, prev_reward, current_reward, delta;
+    double prev_duration, current_duration, prev_reward, current_reward;
 
     //printf("----- simulation----- ");
     //while (!found_optimal_join_order) {  //️ 🐈 simulation_count = executed_chunk
@@ -110,7 +110,9 @@ unique_ptr<QueryResult> SkinnerDB::CreateAndExecuteStatement(){
         // 1 = win
         // 0 = lose
         // 🏆 REWARD FUNCTION C
-        rl_optimizer.Backpropogation(1/current_duration);
+        if (simulation_count != 0) {
+            rl_optimizer.Backpropogation(1/current_duration);
+        }
         //std::cout << "simu_nr." << simulation_count << ", join_order = " << chosen_node->join_node->order_of_relations << " took " << current_duration << "ms, reward=" << (-1)*current_duration << "\n";
 
         // 🏆 REWARD FUNCTION B
@@ -199,7 +201,7 @@ unique_ptr<QueryResult> SkinnerDB::CreateAndExecuteStatement(){
             }
         }
 
-        std::cout << "simu_nr." << simulation_count << ", join_order = " << chosen_node->join_node->order_of_relations << " took " << current_duration << "ms, intermediate = " << delta << ", reward=" << (-1)*current_duration << "\n";
+        std::cout << "simu_nr." << simulation_count << ", join_order = " << chosen_node->join_node->order_of_relations << " took " << current_duration << "ms, reward=" << chosen_node->reward << "\n";
 
         simulation_count += 1;
     }

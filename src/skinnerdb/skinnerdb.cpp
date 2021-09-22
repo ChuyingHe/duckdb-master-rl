735d405477b38e3eb892fad3535fe499bd70145a
Chuying He <heidihcy@outlook.com>
2021-09-22 13:00:28 +0200

    when simulation_count = 0 ignore Backpropogation

--- src/skinnerdb/skinnerdb.cpp
+++ src/skinnerdb/skinnerdb.cpp
@@ -97,7 +97,9 @@ unique_ptr<QueryResult> SkinnerDB::CreateAndExecuteStatement(){
         context.ContinueJoin(lock, query, result, move(bound_values), allow_stream_result, simulation_count);
         current_duration = timer_simulation.check();
 
-        rl_optimizer.Backpropogation((-1)*current_duration);
+        if (simulation_count > 0) {
+           rl_optimizer.Backpropogation((-1)*current_duration);
+	}
 
         if (chosen_node) {
             if (same_order_count>=5 || simulation_count>=20) {

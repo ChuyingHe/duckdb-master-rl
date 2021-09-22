c7747e4be14f6ea654e1d4cc772f4d6158ceab7d
ChuyingHe <heidihcy@outlook.com>
2021-09-22 11:26:27 +0200

    modify GetNodeWithMaxUCT

--- src/skinnerdb/rl_join_order_optimizer.cpp
+++ src/skinnerdb/rl_join_order_optimizer.cpp
@@ -485,7 +485,7 @@ void RLJoinOrderOptimizer::Expansion(JoinRelationSet* union_set, unordered_set<i
 
 NodeForUCT* RLJoinOrderOptimizer::GetNodeWithMaxUCT(NodeForUCT* node) { //case "node->children.empty()" has been eliminated
     NodeForUCT* result;
-    auto max = -1000000000000;
+    double max = -1000000000000;
 
     for (auto const& child:node->children) {
         double avg = child->reward/(child->num_of_visits);
